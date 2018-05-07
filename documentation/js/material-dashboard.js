fc1bbdf8a39bd29a00a556cced636c24308c8e6b
mhsnawan <mohsin.awan0@gmail.com>
2018-05-07 18:55:00 +0500

    Added search bar in user-management table

--- documentation/js/material-dashboard.js
+++ documentation/js/material-dashboard.js
@@ -144,3 +144,29 @@ function debounce(func, wait, immediate) {
 		if (immediate && !timeout) func.apply(context, args);
 	};
 };
+
+function tableData(){
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#example tfoot th').each( function () {
+            var title = $(this).text();
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        } );
+     
+        // DataTable
+        var table = $('#example').DataTable();
+     
+        // Apply the search
+        table.columns().every( function () {
+            var that = this;
+     
+            $( 'input', this.footer() ).on( 'keyup change', function () {
+                if ( that.search() !== this.value ) {
+                    that
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } );
+    } );
+};
